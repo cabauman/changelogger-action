339ff5e1446f6472645706f08e6077c528acd177
cabauman <cab452005@gmail.com>
2022-02-02 15:13:50 +0900

    chore: initial commit

---
+++ tests/integration/stub.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai'
+
+describe('integration.stub', () => {
+  it('1 + 2', () => {
+    expect(1 + 2).to.equal(3)
+  })
+})
---
+++ tests/unit/stub.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai'
+
+describe('unit.stub', () => {
+  it('1 + 2', () => {
+    expect(1 + 2).to.equal(3)
+  })
+})
